80c8f6ae92fb745711e97218a62bea3384cd68a4
KennerStahlman <kenner.stahlman@outlook.com>
2025-04-12 21:04:47 -0500

    done for now

--- game_classes.py
+++ game_classes.py
@@ -1,6 +1,16 @@
 import pygame
 from collections import deque
 
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, width, height, x ,y, color, tile_size):
+        super().__init__()
+        self.tile_size = tile_size
+        self.color = color
+        self.rect = pygame.Rect((x+2)*tile_size, (y+2)*tile_size, width*tile_size, height*tile_size)
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, tile_size, color, grid_width, grid_height):
         super().__init__()
@@ -9,11 +19,95 @@ class Player(pygame.sprite.Sprite):
         self.grid_height = grid_height
         self.color = color
         
-        # Use grid positions (0-19) instead of pixels
-        self.body = [(x, y), (x - 1, y), (x - 2, y), (x - 3, y)]
+        self.body = [(x+2, y+2), (x+1, y+2)]
         self.direction = "right"
         self.living = True
-        self.grow_flag = False  # Controls whether the snake should grow
+        self.grow_flag = False 
+        self.hamiltonian_cycle = None  # Store the cycle
+        self.current_cycle_index = 0 
+        self.generate_hamiltonian_cycle()  # Generate cycle on initialization
+
+    def generate_hamiltonian_cycle(self):
+        x_start = 2
+        y_start = 2
+        x_end = x_start + self.grid_width -1
+        y_end = y_start + self.grid_height -1
+        
+
+        next_pos = {}
+        
+
+        if self.grid_width % 2 == 0:
+            for y in range(y_start, y_end + 1):
+                if (y - y_start) % 2 == 0:
+                    for x in range(x_start, x_end):
+                        next_pos[(x, y)] = (x + 1, y)
+                    next_pos[(x_end, y)] = (x_end, y + 1)
+                else:
+                    for x in range(x_end, x_start, -1):
+                        next_pos[(x, y)] = (x - 1, y)
+                    next_pos[(x_start, y)] = (x_start, y + 1)
+            
+            for y in range(y_end, y_start, -1):
+                next_pos[(x_end, y)] = (x_end, y - 1)
+            
+            next_pos[(x_end, y_start)] = (x_start, y_start)
+        
+        elif self.grid_height % 2 == 0:
+            for x in range(x_start, x_end + 1):
+                if (x - x_start) % 2 == 0:
+                    for y in range(y_start, y_end):
+                        next_pos[(x, y)] = (x, y + 1)
+                    next_pos[(x, y_end)] = (x + 1, y_end)
+                else:
+                    for y in range(y_end, y_start, -1):
+                        next_pos[(x, y)] = (x, y - 1)
+                    next_pos[(x, y_start)] = (x + 1, y_start)
+            
+            for x in range(x_end, x_start, -1):
+                next_pos[(x, y_end)] = (x - 1, y_end)
+            
+            next_pos[(x_start, y_end)] = (x_start, y_start)
+        
+        self.hamiltonian_cycle = next_pos
+
+    def follow_hamiltonian_cycle(self, width, height, obstacles):
+        """Use the Hamiltonian cycle to determine next move."""
+        head_x, head_y = self.body[0]
+        
+        rel_x = head_x - 2
+        rel_y = head_y - 2
+        
+        width = width
+        height = height
+        print(self.body[0])
+        print(self.hamiltonian_cycle[self.body[0]])
+        if self.body[0][0] > self.hamiltonian_cycle[self.body[0]][0]:
+            self.direction == 'left'
+        if self.body[0][0] < self.hamiltonian_cycle[self.body[0]][0]:
+            self.direction == 'right'
+        if self.body[0][1] > self.hamiltonian_cycle[self.body[0]][1]:
+            self.direction == 'up'
+        if self.body[0][1] < self.hamiltonian_cycle[self.body[0]][1]:
+            self.direction == 'down'
+        if self.direction:
+            if self.direction == 'left':
+                head_x -= 1
+            elif self.direction == 'right':
+                head_x += 1
+            elif self.direction == 'up':
+                head_y -= 1
+            elif self.direction == 'down':
+                head_y += 1
+
+            self.body.insert(0, (head_x, head_y))
+            self.collisions(obstacles)
+            if self.living == False:
+                self.body.remove((head_x, head_y))
+            elif not self.grow_flag:
+                self.body.pop()
+            else:
+                self.grow_flag = False
 
     def flood_fill(self, start_pos):
         """Returns the number of open spaces reachable from start_pos."""
@@ -29,7 +123,7 @@ class Player(pygame.sprite.Sprite):
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nx, ny = x + dx, y + dy
 
-                if (0 <= nx < self.grid_width and 0 <= ny < self.grid_height and
+                if (1 < nx < self.grid_width+2 and 1 < ny < self.grid_height+2 and
                     (nx, ny) not in set(self.body) and (nx, ny) not in visited):
                     queue.append((nx, ny))
                     visited.add((nx, ny))
@@ -42,7 +136,7 @@ class Player(pygame.sprite.Sprite):
         visited = set()
         visited.add(start_pos)
 
-        tail = self.body[-1]  # Allow movement into the last tail position
+        tail = self.body[-1]
 
         while queue:
             x, y = queue.popleft()
@@ -52,13 +146,13 @@ class Player(pygame.sprite.Sprite):
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nx, ny = x + dx, y + dy
 
-                if (0 <= nx < self.grid_width and 0 <= ny < self.grid_height and
+                if (1 < nx < self.grid_width+2 and 1 < ny < self.grid_height+2 and
                     ((nx, ny) not in set(self.body) or (nx, ny) == tail) and
                     (nx, ny) not in visited):
                     queue.append((nx, ny))
                     visited.add((nx, ny))
 
-        return False  # No path found to the food
+        return False
 
     def bfs(self, food_position):
         """Determines the best move using BFS-based logic with flood fill and path checking."""
@@ -66,9 +160,8 @@ class Player(pygame.sprite.Sprite):
             return
 
         head_x, head_y = self.body[0]
-        body_positions = set(self.body[1:])  # Keep entire body except head
+        body_positions = set(self.body[1:])
 
-        # Define possible moves
         moves = {
             "left":  (head_x - 1, head_y),
             "right": (head_x + 1, head_y),
@@ -76,30 +169,26 @@ class Player(pygame.sprite.Sprite):
             "down":  (head_x, head_y + 1)
         }
 
-        # Compute valid moves
         valid_moves = {dir: pos for dir, pos in moves.items()
-                       if pos not in body_positions and 0 <= pos[0] < self.grid_width and 0 <= pos[1] < self.grid_height}
+                       if pos not in body_positions and 1 < pos[0] < self.grid_width+2 and 1 < pos[1] < self.grid_height+2}
         
         if not valid_moves:
             self.living = False
-            return  # Snake stops moving and dies
+            return
 
         if not valid_moves:
-            return  # Do nothing if no valid moves exist
-
-        # 1️⃣ Compute flood fill for each valid move
+            return 
         best_move = None
         max_space = -1
 
         for direction, position in valid_moves.items():
             open_space = self.flood_fill(position)
 
-            # Check if this move keeps the path to food open
+
             if open_space > max_space and self.path_to_food(position, food_position):  
                 max_space = open_space
-                best_move = direction  # Choose move with max open space that still allows a path to food
+                best_move = direction 
 
-        # 2️⃣ Try greedy move first if it's safe
         greedy_move = None
         if head_x > food_position[0] and "left" in valid_moves:
             greedy_move = "left"
@@ -114,9 +203,9 @@ class Player(pygame.sprite.Sprite):
         if greedy_move and greedy_move in valid_moves:
             future_space = self.flood_fill(valid_moves[greedy_move])
             if best_move and future_space < max_space:
-                self.direction = best_move  # Flood fill is better
+                self.direction = best_move
             elif future_space > len(self.body) * 1.5 and self.path_to_food(valid_moves[greedy_move], food_position):
-                self.direction = greedy_move  # Greedy move is safe
+                self.direction = greedy_move
             else:
                 self.direction = best_move if best_move else max(valid_moves, key=lambda d: self.flood_fill(valid_moves[d]))
 
@@ -126,7 +215,7 @@ class Player(pygame.sprite.Sprite):
 
 
         elif valid_moves:
-            self.direction = max(valid_moves, key=lambda d: self.flood_fill(valid_moves[d]))  # Pick the safest move
+            self.direction = max(valid_moves, key=lambda d: self.flood_fill(valid_moves[d]))
 
         if not self.direction:
             self.living = False
@@ -152,115 +241,116 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.grow_flag = False  
 
-    # def better_greedy(self, food_position):
-    #     if self.living:
+    def better_greedy(self, food_position):
+        if self.living:
             
-    #         head_x, head_y = self.body[0]
+            head_x, head_y = self.body[0]
 
-    #         body_positions = set(self.body[1:])
+            body_positions = set(self.body[1:])
 
-    #         moves = {
-    #             "left":  (head_x - 1, head_y),
-    #             "right": (head_x + 1, head_y),
-    #             "up":    (head_x, head_y - 1),
-    #             "down":  (head_x, head_y + 1)
-    #         }
+            moves = {
+                "left":  (head_x - 1, head_y),
+                "right": (head_x + 1, head_y),
+                "up":    (head_x, head_y - 1),
+                "down":  (head_x, head_y + 1)
+            }
 
-    #         valid_moves = {dir for dir, pos in moves.items() if pos not in body_positions and 0 <= pos[0] < self.grid_width and 0<= pos[1] < self.grid_height}
-    #         print(valid_moves)
+            valid_moves = {dir for dir, pos in moves.items() if pos not in body_positions and 0 <= pos[0] < self.grid_width and 0<= pos[1] < self.grid_height}
+            print(valid_moves)
             
-    #         if self.body[0][0] > food_position[0] and "left" in valid_moves:
-    #             self.direction = "left"
-    #         elif self.body[0][0] < food_position[0] and "right" in valid_moves:
-    #             self.direction = "right"
-    #         elif self.body[0][1] > food_position[1] and "up" in valid_moves:
-    #             self.direction = "up"
-    #         elif self.body[0][1] < food_position[1] and "down" in valid_moves:
-    #             self.direction = "down"
+            if self.body[0][0] > food_position[0] and "left" in valid_moves:
+                self.direction = "left"
+            elif self.body[0][0] < food_position[0] and "right" in valid_moves:
+                self.direction = "right"
+            elif self.body[0][1] > food_position[1] and "up" in valid_moves:
+                self.direction = "up"
+            elif self.body[0][1] < food_position[1] and "down" in valid_moves:
+                self.direction = "down"
             
-    #         if self.direction not in valid_moves and valid_moves:
-    #             self.direction = next(iter(valid_moves))
-    #         if self.direction:
+            if self.direction not in valid_moves and valid_moves:
+                self.direction = next(iter(valid_moves))
+            if self.direction:
 
-    #             if self.direction == 'left':
-    #                 head_x -= 1
-    #             elif self.direction == 'right':
-    #                 head_x += 1
-    #             elif self.direction == 'up':
-    #                 head_y -= 1
-    #             elif self.direction == 'down':
-    #                 head_y += 1
+                if self.direction == 'left':
+                    head_x -= 1
+                elif self.direction == 'right':
+                    head_x += 1
+                elif self.direction == 'up':
+                    head_y -= 1
+                elif self.direction == 'down':
+                    head_y += 1
 
-    #             self.body.insert(0, (head_x, head_y))
+                self.body.insert(0, (head_x, head_y))
 
-    #             if not self.grow_flag:
-    #                 self.body.pop() 
-    #             else:
-    #                 self.grow_flag = False  
+                if not self.grow_flag:
+                    self.body.pop() 
+                else:
+                    self.grow_flag = False  
 
-    # def greedy_algo(self, food_position):
-    #     if self.living:
-    #         if self.body[0][0] > food_position[0] and self.direction != "right" :
-    #             self.direction = "left"
-    #         if self.body[0][0] < food_position[0] and self.direction != "left":
-    #             self.direction = "right"
-    #         if self.body[0][1] > food_position[1] and self.direction != "down":
-    #             self.direction = "up"
-    #         if self.body[0][1] < food_position[1] and self.direction != "up":
-    #             self.direction = "down"
+    def greedy_algo(self, food_position):
+        if self.living:
+            if self.body[0][0] > food_position[0] and self.direction != "right" :
+                self.direction = "left"
+            if self.body[0][0] < food_position[0] and self.direction != "left":
+                self.direction = "right"
+            if self.body[0][1] > food_position[1] and self.direction != "down":
+                self.direction = "up"
+            if self.body[0][1] < food_position[1] and self.direction != "up":
+                self.direction = "down"
             
-    #         if self.direction:
-    #             head_x, head_y = self.body[0]  # Get the head position
+            if self.direction:
+                head_x, head_y = self.body[0]
 
-    #             if self.direction == 'left':
-    #                 head_x -= 1
-    #             elif self.direction == 'right':
-    #                 head_x += 1
-    #             elif self.direction == 'up':
-    #                 head_y -= 1
-    #             elif self.direction == 'down':
-    #                 head_y += 1
+                if self.direction == 'left':
+                    head_x -= 1
+                elif self.direction == 'right':
+                    head_x += 1
+                elif self.direction == 'up':
+                    head_y -= 1
+                elif self.direction == 'down':
+                    head_y += 1
 
-    #             self.body.insert(0, (head_x, head_y))
+                self.body.insert(0, (head_x, head_y))
 
-    #             if not self.grow_flag:
-    #                 self.body.pop()  
-    #             else:
-    #                 self.grow_flag = False 
-
-
-    # def move(self, keys):
-    #     if self.living:
-    #         if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.direction != "right":
-    #             self.direction = "left"
-    #         elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.direction != "left":
-    #             self.direction = "right"
-    #         elif (keys[pygame.K_UP] or keys[pygame.K_w]) and self.direction != "down":
-    #             self.direction = "up"
-    #         elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.direction != "up":
-    #             self.direction = "down"
-            
-
-    #         if self.direction:
-    #             head_x, head_y = self.body[0]  # Get the head position
-
-    #             if self.direction == 'left':
-    #                 head_x -= 1
-    #             elif self.direction == 'right':
-    #                 head_x += 1
-    #             elif self.direction == 'up':
-    #                 head_y -= 1
-    #             elif self.direction == 'down':
-    #                 head_y += 1
+                if not self.grow_flag:
+                    self.body.pop()  
+                else:
+                    self.grow_flag = False 
 
 
-    #             self.body.insert(0, (head_x, head_y))
+    def move(self, keys,obstacles):
+        self.collisions(obstacles)
+        if self.living:
+            if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.direction != "right":
+                self.direction = "left"
+            elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.direction != "left":
+                self.direction = "right"
+            elif (keys[pygame.K_UP] or keys[pygame.K_w]) and self.direction != "down":
+                self.direction = "up"
+            elif (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.direction != "up":
+                self.direction = "down"
 
 
-    #             if not self.grow_flag:
-    #                 self.body.pop()
-    #             else:
-    #                 self.grow_flag = False
+            if self.direction:
+                head_x, head_y = self.body[0]
+
+                if self.direction == 'left':
+                    head_x -= 1
+                elif self.direction == 'right':
+                    head_x += 1
+                elif self.direction == 'up':
+                    head_y -= 1
+                elif self.direction == 'down':
+                    head_y += 1
+
+                self.body.insert(0, (head_x, head_y))
+                self.collisions(obstacles)
+                if not self.living:
+                    self.body.remove((head_x, head_y))
+                if not self.grow_flag:
+                    self.body.pop()
+                else:
+                    self.grow_flag = False
 
     def draw(self, screen):
 
@@ -268,11 +358,13 @@ class Player(pygame.sprite.Sprite):
             x, y = segment[0] * self.tile_size, segment[1] * self.tile_size
             pygame.draw.rect(screen, self.color, (x, y, self.tile_size, self.tile_size))
 
-    def collisions(self):
+    def collisions(self, obstacles):
         head_x, head_y = self.body[0]
+        for obstacle in obstacles:
+            if pygame.Rect.collidepoint(obstacle.rect, head_x*self.tile_size, head_y*self.tile_size):
+                self.living = False
 
-
-        if head_x < 0 or head_x >= self.grid_width or head_y < 0 or head_y >= self.grid_height:
+        if head_x < 2 or head_x > self.grid_width+1 or head_y < 2 or head_y > self.grid_height+1:
             self.living = False
 
 
@@ -292,9 +384,10 @@ class Food(pygame.sprite.Sprite):
         self.color = color
         
 
-        self.position = (x, y) 
+        self.position = (x+2, y+2) 
 
 
     def draw(self, screen):
         x, y = self.position[0] * self.tile_size, self.position[1] * self.tile_size
         pygame.draw.rect(screen, self.color, (x, y, self.tile_size, self.tile_size))
+
--- snake.py
+++ snake.py
@@ -3,69 +3,132 @@ import pygame
 import game_classes
 import random
 
+
 pygame.init()
 
-tile_size = 30
-columns = 20
-rows = 20
+tile_size = 50
+columns = 10
+rows = 10
 width, height = rows * tile_size, columns * tile_size
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width+tile_size*4, height+tile_size*4))
 clock = pygame.time.Clock()
 game_active = True
+obs_on = True
+
+class Board(pygame.sprite.Sprite):
+    def __init__(self,):
+        super().__init__()
+        self.board = [(j,i) for i in range(rows) for j in range(columns)]
+        self.darktiles = [i for i in self.board if (i[0]+i[1]+2)%2 != 0]
+        self.lighttiles = [i for i in self.board if (i[0]+i[1]+2)%2 == 0 ]
+        self.darktiles_rect = [pygame.Rect(i[0]*tile_size+100, i[1]*tile_size+100, tile_size, tile_size) for i in self.darktiles]
+        self.lighttiles_rect = [pygame.Rect(i[0]*tile_size+100, i[1]*tile_size+100, tile_size, tile_size) for i in self.lighttiles]
+    def draw(self,screen):  
+        [pygame.draw.rect(screen, 'light grey', self.darktiles_rect[i]) for i in range(columns*rows//2)]
+        [pygame.draw.rect(screen, 'dark grey', self.lighttiles_rect[i]) for i in range(columns*rows//2)]
 
 font = pygame.font.Font(None, 50)
 
 def reset_game():
     """Resets the game variables and restarts the game."""
-    global player, food,all_sprites, frame_count
-    player = game_classes.Player(5, 10, tile_size, 'green', rows, columns)
-    food = game_classes.Food(9,9,tile_size,'red', rows, columns)
+    global player, food, all_sprites, frame_count, board, obstacles
+    
+
+    board = Board()
+    player = game_classes.Player(2, 5, tile_size, 'green', rows, columns)
+    food = game_classes.Food(8, 5, tile_size, 'red', rows, columns)
+    
+    if obs_on:
+        reserved_positions = [(2+2, 5+2), (1+2, 5+2), (3+2, 5+2), (8+2, 5+2)]
+
+        obs_count = random.randint(1, 5)
+        obstacles = []
+        
+        while len(obstacles) < obs_count:
+
+            width = random.randint(1, 3)
+            height = random.randint(1, 3)
+            x = random.randint(0, 9)
+            y = random.randint(0, 9)
+
+            if x + width > 9 or y + height > 9:
+                continue
+                
+            new_obstacle = game_classes.Obstacle(width, height, x, y, 'orange', tile_size)
+
+            collision = False
+            for pos in reserved_positions:
+                if pygame.Rect.collidepoint(new_obstacle.rect, pos[0]*tile_size, pos[1]*tile_size):
+                    collision = True
+                    break
+
+            for existing_obs in obstacles:
+                if pygame.Rect.colliderect(new_obstacle.rect, existing_obs.rect):
+                    collision = True
+                    break
+                    
+            if not collision:
+                obstacles.append(new_obstacle)
+    else:
+        obstacles = []
+
     all_sprites = pygame.sprite.Group()
-    all_sprites.add(food ,player)
-    frame_count = 0  
+    all_sprites.add(board, food, player, *obstacles)
+    frame_count = 0
 
 
 
-FPS = 60
+FPS = 10
 MOVE_DELAY = 1
 frame_count = 0
 
 
 reset_game()
 
+
+
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not player.living:
             reset_game()
-
-    screen.fill((0, 0, 0))
-
     keys = pygame.key.get_pressed()
 
-    if player.living:
+    player.collisions(obstacles)
+
+    if player.living and frame_count%MOVE_DELAY == 0:
         if player.body[0] == food.position:
             while food.position in player.body:
-                food.position = (random.randint(0, rows-1), random.randint(0,columns-1))
+                food.position = (random.randint(2, rows+1), random.randint(2,columns+1))
+                for obstacle in obstacles:
+                    while pygame.Rect.collidepoint(obstacle.rect, food.position[0]*tile_size, food.position[1]*tile_size):
+                        food.position = (random.randint(2, rows+1), random.randint(2,columns+1))
             player.grow_flag = True
-
-        player.bfs(food.position)
-        # player.better_greedy(food.position)
+        # player.follow_hamiltonian_cycle(rows, columns, obstacles)
+        # player.bfs(food.position)
         # player.greedy_algo(food.position)
-        # player.move(keys)
-        player.collisions()
-
-
+        player.move(keys,obstacles)
+    screen.fill((0, 0, 0))
 
     for sprite in all_sprites:
         sprite.draw(screen)
+    score_message = font.render(f"score: {len(player.body)}", 0, 'red')
+    score_rect = score_message.get_rect(topleft=(width//2+25, 50))
+    screen.blit(score_message, score_rect)
+    fps_message = font.render(f"mps: {round(clock.get_fps())}", True, 'red')
+    screen.blit(fps_message, (10,10))
+        
+    if len(player.body) == rows * columns:
+        death_message = font.render("You won, Press SPACE to restart", True, 'white')
+        screen.blit(death_message, (0, rows*tile_size/2))
+        player.living = False
 
-    if not player.living:
+    elif not player.living:
         death_message = font.render("You died - Press SPACE to restart", True, 'white')
-        screen.blit(death_message, (10, 10))
-
+        screen.blit(death_message, (columns*tile_size/2, rows*tile_size/2))
+    frame_count+=1
     pygame.display.flip()
     clock.tick(FPS)
-    frame_count += 1
